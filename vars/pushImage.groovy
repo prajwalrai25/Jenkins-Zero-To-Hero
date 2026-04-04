40637a225377b16f806ec00090ac65c8fed51dd5
prajwalrai25 <49466513+prajwalrai25@users.noreply.github.com>
2026-04-04 12:02:34 +0530

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -2,6 +2,6 @@ def call(String imageName) {
   docker.withRegistry('https://index.docker.io/v1/', 'docker-cred'){
     sh """
     echo "push to repo"
-    docker push imageName
+    docker push ${imageName}
     """
   }
